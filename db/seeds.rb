54318fdd1a872c4bf88876a9df01b66ac50cad7d
MadisonRong <rongweixin041@gmail.com>
2015-04-07 15:58:38 +0800

    update dish import way

--- db/seeds.rb
+++ db/seeds.rb
@@ -38,23 +38,23 @@ DishType.create!(name: "河粉")
 DishType.create!(name: "米粉")
 DishType.create!(name: "意粉类")
 # dish
-require 'roo'
-s = Roo::Excelx.new("/Users/MadisonRong/BurgerBitch/db/dishes.xlsx")
-sheets_count = s.sheets.count - 1
-0.upto(sheets_count) do |sheet|
-  s.default_sheet = s.sheets[sheet]
-  row_count = s.last_row - 1
-  # create restaurant
-  restaurant = Restaurant.create!(name: s.cell(1,1), phone: s.cell(s.last_row, 1))
-  3.upto(row_count) do |row|
-    next if s.cell(row, 1).nil?
-    # create dish
-    puts "#{s.cell(row, 1)} #{s.cell(row, 2).sub('元', '')} #{s.cell(row, 3)}"
-    Dish.create!(
-      name: s.cell(row, 1), 
-      price: s.cell(row, 2).sub('元', '').to_i, 
-      dish_type_id: DishType.find_or_create_by(name: s.cell(row, 3)).id,
-      restaurant_id: restaurant.id
-    )
-  end
-end
+# require 'roo'
+# s = Roo::Excelx.new("BurgerBitch/db/dishes.xlsx")
+# sheets_count = s.sheets.count - 1
+# 0.upto(sheets_count) do |sheet|
+#   s.default_sheet = s.sheets[sheet]
+#   row_count = s.last_row - 1
+#   # create restaurant
+#   restaurant = Restaurant.create!(name: s.cell(1,1), phone: s.cell(s.last_row, 1))
+#   3.upto(row_count) do |row|
+#     next if s.cell(row, 1).nil?
+#     # create dish
+#     puts "#{s.cell(row, 1)} #{s.cell(row, 2).sub('元', '')} #{s.cell(row, 3)}"
+#     Dish.create!(
+#       name: s.cell(row, 1), 
+#       price: s.cell(row, 2).sub('元', '').to_i, 
+#       dish_type_id: DishType.find_or_create_by(name: s.cell(row, 3)).id,
+#       restaurant_id: restaurant.id
+#     )
+#   end
+# end
