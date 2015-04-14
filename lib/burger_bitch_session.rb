a39b9e31a420c27e5e53b7a6a911292b867496c3
MadisonRong <rongweixin041@gmail.com>
2015-04-14 14:44:27 +0800

    deploy: change callback url in welcome page

--- lib/burger_bitch_session.rb
+++
@@ -1,9 +0,0 @@
-class BurgerBitchSession
-  def self.create(value)
-    cookies[:burgerbitch] = {
-      value: value,
-      expires: 1.week.from_now,
-      domain: '172.16.10.26'
-    }
-  end
-end
