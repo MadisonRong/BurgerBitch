e58c40787e77eaa3dc3b353acd6fa1c9434abee3
MadisonRong <rongweixin041@gmail.com>
2015-04-24 13:21:29 +0800

    test

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -7,7 +7,7 @@ class Restaurant < ActiveRecord::Base
 
   ***REMOVED*** 根据餐厅获取对应菜单
   scope :get_dishes, ->(restaurant_id){
-    dishes = Restaurant.find(restaurant_id).dishes.includes("dish_type").order('id, dish_type_id, price')
+    dishes = Restaurant.find(restaurant_id).dishes.includes("dish_type").order('dish_type_id, id, price')
     ***REMOVED*** 分类展示菜式
     dishes_hash = Hash.new
     iterator = 0
@@ -25,29 +25,30 @@ class Restaurant < ActiveRecord::Base
       if iterator + 1 == dishes_size
         logger.info "the last one is: ***REMOVED***{dishes[iterator].name}***REMOVED******REMOVED***{dishes[iterator].dish_type.name}"
         ***REMOVED*** 最后一个元素，给hash赋值并加入数组，同时防止数组越界
-        ***REMOVED*** if dishes[iterator].dish_type.name != dishes[iterator - 1].dish_type.name && result_hash[dishes[iterator].dish_type.name.to_s].nil? 
+        if result_hash[dishes[iterator].dish_type.name.to_s].nil? 
+          result_hash[dishes[iterator].dish_type.name.to_s] = result_array
+        else
+          result_hash[dishes[iterator].dish_type.name.to_s] += result_array
+        end
+        result_array << dishes[iterator]
+        ***REMOVED*** 判断当前菜式的类型是否与前一个不相同
+        ***REMOVED*** if dishes[iterator].dish_type.name != dishes[iterator - 1].dish_type.name
+        ***REMOVED***   ***REMOVED*** 不相同，将前面积累的数组赋值给对应的hash
+        ***REMOVED***   if result_hash[dishes[iterator - 1].dish_type.name.to_s].nil?
+        ***REMOVED***     result_hash[dishes[iterator - 1].dish_type.name.to_s] = result_array
+        ***REMOVED***   else
+        ***REMOVED***     result_hash[dishes[iterator - 1].dish_type.name.to_s] += result_array
+        ***REMOVED***   end
+        ***REMOVED***   ***REMOVED*** 开辟新数组储存接下来的元素
+        ***REMOVED***   result_array = Array.new
+        ***REMOVED***   ***REMOVED*** 将当前值加入数组
+        ***REMOVED***   result_array << dishes[iterator]
         ***REMOVED***   result_hash[dishes[iterator].dish_type.name.to_s] = result_array
         ***REMOVED*** else
-        ***REMOVED***   result_hash[dishes[iterator].dish_type.name.to_s] += result_array
+        ***REMOVED***   ***REMOVED*** 相同则直接加入数组，并且将数组赋值给hash
+        ***REMOVED***   result_array << dishes[iterator]
+        ***REMOVED***   result_hash[dishes[iterator - 1].dish_type.name.to_s] = result_array
         ***REMOVED*** end
-        ***REMOVED*** result_array << dishes[iterator]
-        ***REMOVED*** 判断当前菜式的类型是否与前一个不相同
-        if dishes[iterator].dish_type.name != dishes[iterator - 1].dish_type.name
-          ***REMOVED*** 不相同，将前面积累的数组赋值给对应的hash
-          if result_hash[dishes[iterator - 1].dish_type.name.to_s].nil?
-            result_hash[dishes[iterator - 1].dish_type.name.to_s] = result_array
-          else
-            result_hash[dishes[iterator - 1].dish_type.name.to_s] += result_array
-          end
-          ***REMOVED*** 开辟新数组储存接下来的元素
-          result_array = Array.new
-          ***REMOVED*** 将当前值加入数组
-          result_array << dishes[iterator]
-        else
-          ***REMOVED*** 相同则直接加入数组，并且将数组赋值给hash
-          result_array << dishes[iterator]
-          result_hash[dishes[iterator - 1].dish_type.name.to_s] = result_array
-        end
         iterator += 1
         next
       end
