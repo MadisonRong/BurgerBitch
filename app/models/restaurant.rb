d19cebf3c8688ad2eb05fa8b1b293d82ae4c2cd5
MadisonRong <rongweixin041@gmail.com>
2015-04-24 13:05:36 +0800

    test

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -25,12 +25,28 @@ class Restaurant < ActiveRecord::Base
       if iterator + 1 == dishes_size
         logger.info "the last one is: #{dishes[iterator].name}##{dishes[iterator].dish_type.name}"
         # 最后一个元素，给hash赋值并加入数组，同时防止数组越界
-        if dishes[iterator].dish_type.name != dishes[iterator - 1].dish_type.name && result_hash[dishes[iterator].dish_type.name.to_s].nil? 
-          result_hash[dishes[iterator].dish_type.name.to_s] = result_array
+        # if dishes[iterator].dish_type.name != dishes[iterator - 1].dish_type.name && result_hash[dishes[iterator].dish_type.name.to_s].nil? 
+        #   result_hash[dishes[iterator].dish_type.name.to_s] = result_array
+        # else
+        #   result_hash[dishes[iterator].dish_type.name.to_s] += result_array
+        # end
+        # result_array << dishes[iterator]
+        # 判断当前菜式的类型是否与前一个不相同
+        if dishes[iterator].dish_type.name != dishes[iterator - 1].dish_type.name
+          # 不相同，将前面积累的数组赋值给对应的hash
+          if result_hash[dishes[iterator - 1].dish_type.name.to_s].nil?
+            result_hash[dishes[iterator - 1].dish_type.name.to_s] = result_array
+          else
+            result_hash[dishes[iterator - 1].dish_type.name.to_s] += result_array
+          end
+          # 开辟新数组储存接下来的元素
+          result_array = Array.new
+          # 将当前值加入数组
+          result_array << dishes[iterator]
         else
-          result_hash[dishes[iterator].dish_type.name.to_s] += result_array
+          # 相同则直接加入数组，直至不相同才将数组赋值给hash
+          result_array << dishes[iterator]
         end
-        result_array << dishes[iterator]
         iterator += 1
         next
       end
