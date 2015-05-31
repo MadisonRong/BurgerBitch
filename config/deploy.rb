d9f59c53c30c6ab141264f6be4e64e4392ba0d9b
MadisonRong <rongweixin041@gmail.com>
2015-05-31 10:53:33 +0800

    ready to re-deploy

--- config/deploy.rb
+++ config/deploy.rb
@@ -31,10 +31,10 @@ set :rvm_ruby_string, '1.26.11'
 # set :pty, true
 
 # Default value for :linked_files is []
-# set :linked_files, fetch(:linked_files, []).push('config/database.yml')
+set :linked_files, fetch(:linked_files, []).push('config/database.yml')
 
 # Default value for linked_dirs is []
-# set :linked_dirs, fetch(:linked_dirs, []).push('bin', 'log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system')
+set :linked_dirs, fetch(:linked_dirs, []).push('bin', 'log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system')
 
 # Default value for default_env is {}
 # set :default_env, { path: "/opt/ruby/bin:$PATH" }
