b07ee825187c1c9745b241932bf5e90bd41b89e7
MadisonRong <rongweixin041@gmail.com>
2015-05-31 11:34:50 +0800

    ready to re-deploy

--- config/deploy.rb
+++ config/deploy.rb
@@ -31,7 +31,7 @@ set :rvm_ruby_string, '1.26.11'
 # set :pty, true
 
 # Default value for :linked_files is []
-set :linked_files, fetch(:linked_files, []).push('config/database.yml')
+set :linked_files, fetch(:linked_files, []).push('config/database.yml', 'config/secrets.yml')
 
 # Default value for linked_dirs is []
 set :linked_dirs, fetch(:linked_dirs, []).push('bin', 'log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system')
